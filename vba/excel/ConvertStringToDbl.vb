89e407f388c992a537b95cc82e7f9bbd3d4ea920
Vladimir Petrigo <taenaru@gmail.com>
2017-01-17 11:19:11 +0300

    Clean up

--- vba/excel/ConvertStringToDbl.vb
+++ vba/excel/ConvertStringToDbl.vb
@@ -1,15 +1,19 @@
 Sub ConvertCellsToNumeric()
+	' Range of columns
     For col = 11 To 12:
         Dim i As Integer
         
+        ' Row to start from 
         i = 10
-        
+  
         Do While ActiveSheet.Cells(i, col).Value <> ""
             ActiveSheet.Cells(i, col).Select
             
             With ActiveCell
                 .Value = CDbl(.Value)
             End With
+            
+            i = i + 1
         Loop
     Next col
 End Sub
